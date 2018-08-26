12f25209e53e10a53be0121bfb1ef355ebba8434
matus <bartko.mat@gmail.com>
2018-08-26 18:57:02 +0200

    orignal working version

---
+++ cloudformation/create-webhooks-stack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+aws cloudformation create-stack \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --region eu-west-1 \
+    --stack-name wixit-auth-webhooks \
+    --template-body file://webhooks.yaml \
+    --parameters file://webhooks-params.json
