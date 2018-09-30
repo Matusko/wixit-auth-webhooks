39e090cd6e6f688c8f5f99d2da60095bb7fd1df7
matus <bartko.mat@gmail.com>
2018-09-30 11:33:53 +0200

    working version

--- functions/source/GitPullS3/lambda_function.py
+++ functions/source/GitPullS3/lambda_function.py
@@ -6,7 +6,7 @@
 #  See the License for the specific language governing permissions and limitations under the License.
 
 from pygit2 import Keypair, discover_repository, Repository, clone_repository, RemoteCallbacks
-from boto3 import client
+from boto3 import client, resource
 import os
 import stat
 import shutil
@@ -15,6 +15,36 @@ from ipaddress import ip_network, ip_address
 import logging
 import hmac
 import hashlib
+import botocore
+import json
+import threading
+
+s3 = client('s3', region_name='eu-west-1')
+cloudformation = client('cloudformation', region_name='eu-west-1')
+branches_deployment_state_file = 'branches_deployment_state.json'
+webhhoks_configs_s3_bucket_name = 'webhooks-configs-and-templates'
+configs = {
+    'wixit-auth-infrastructure': {
+        'params': 'wixit-auth-infrastructure/microservice-pipeline-params-without-variables.json',
+        'template': 'wixit-auth-infrastructure/microservice-pipeline.yaml',
+        'stack-name-prefix': 'wixit-auth-infrastructure',
+        'branch-key': 'GitHubInfrastructureBranch',
+        'artifact-store-key': 'ArtifactStoreS3Location',
+        'context-path-key': 'ContextPath',
+        'infrastructure-key': '',
+        'infrastructure': ''
+    },
+    'wixit-auth': {
+        'params': 'wixit-auth/deployment-pipeline-params-without-variables.json',
+        'template': 'wixit-auth/deployment-pipeline.yaml',
+        'stack-name-prefix': 'wixit-auth',
+        'branch-key': 'GitHubBranch',
+        'artifact-store-key': 'MicroserviceArtifactStoreS3Location',
+        'context-path-key': '',
+        'infrastructure-key': 'AuthInfrastructureStackName',
+        'infrastructure': 'wixit-auth-infrastructure'
+    }
+}
 
 # If true the function will not include .git folder in the zip
 exclude_git = True
@@ -31,8 +61,146 @@ logger.handlers[0].setFormatter(logging.Formatter('[%(asctime)s][%(levelname)s] 
 logging.getLogger('boto3').setLevel(logging.ERROR)
 logging.getLogger('botocore').setLevel(logging.ERROR)
 
-s3 = client('s3')
 kms = client('kms')
+
+def get_branch_name_key_part(branch_name):
+    branch_name_key_part = branch_name
+    branch_name_prefix_part = ''
+    if "_" in branch_name_key_part:
+        branch_name_prefix_part, branch_name_key_part = branch_name_key_part.split("_", 1)
+
+    return branch_name_prefix_part, branch_name_key_part
+
+def get_artifacts_bucket_name(repo, branch_name_key_part):
+    return repo + '-' + branch_name_key_part + "-artifacts"
+
+def stack_set_name(repo, branch_name_key_part, suffix):
+    return configs[repo]['stack-name-prefix'] + '-' + branch_name_key_part + '-' + suffix
+
+def create_stack(repo, branch_name):
+    s3.download_file(webhhoks_configs_s3_bucket_name, branches_deployment_state_file, '/tmp/' + branches_deployment_state_file)
+
+    with open('/tmp/' + branches_deployment_state_file) as file:
+        branches_deployment_state = json.load(file)
+    config = configs[repo]
+    if (repo in branches_deployment_state and branch_name in branches_deployment_state[repo]):
+        print ('branch already deployed')
+    elif (config['infrastructure'] and (config['infrastructure'] not in branches_deployment_state or branch_name not in branches_deployment_state[config['infrastructure']])):
+        print ('infrastructure dependency not satisfied')
+    else:
+        branch_name_prefix_part, branch_name_key_part = get_branch_name_key_part(branch_name)
+
+        if branch_name_prefix_part == 'feature':
+
+            template_file_s3 = config['template']
+            param_file_s3 = config['params']
+            template_file = '/tmp/' + os.path.basename(template_file_s3)
+            param_file = '/tmp/' + os.path.basename(param_file_s3)
+            stack_name = config['stack-name-prefix'] + '-' + branch_name_key_part
+
+            s3.download_file(webhhoks_configs_s3_bucket_name, template_file_s3, template_file)
+            s3.download_file(webhhoks_configs_s3_bucket_name, param_file_s3, param_file)
+
+            with open(param_file) as f:
+                data = json.load(f)
+            if config['branch-key']:
+                branch_param = {
+                    "ParameterKey": config['branch-key'],
+                    "ParameterValue": branch_name
+                }
+                data.append(branch_param)
+
+            if config['artifact-store-key']:
+                templates_s3_bucket_param = {
+                    "ParameterKey": config['artifact-store-key'],
+                    "ParameterValue": get_artifacts_bucket_name(repo, branch_name_key_part)
+                }
+                data.append(templates_s3_bucket_param)
+
+            if config['context-path-key']:
+                context_path_param = {
+                    "ParameterKey": config['context-path-key'],
+                    "ParameterValue": "/" + branch_name_key_part
+                }
+                data.append(context_path_param)
+
+            if config['infrastructure-key']:
+                context_path_param = {
+                    "ParameterKey": config['infrastructure-key'],
+                    "ParameterValue": config['infrastructure'] + '-' + branch_name_key_part
+                }
+                data.append(context_path_param)
+
+            with open(template_file) as yaml_data:
+                template = yaml_data.read()
+
+            cloudformation.create_stack(StackName = stack_name, TemplateBody = template, Parameters = data, Capabilities = ['CAPABILITY_NAMED_IAM',])
+
+            os.remove(template_file)
+            os.remove(param_file)
+
+            if (repo in branches_deployment_state):
+                branches_deployment_state[repo].append(branch_name)
+            else:
+                branches_deployment_state[repo] = [branch_name,]
+
+            with open('/tmp/' + branches_deployment_state_file, 'w') as outfile:
+                json.dump(branches_deployment_state, outfile)
+            s3.upload_file('/tmp/' + branches_deployment_state_file, webhhoks_configs_s3_bucket_name, branches_deployment_state_file)
+
+    os.remove('/tmp/' + branches_deployment_state_file)
+
+def delete_bucket(bucket_name):
+    s3_resource = resource('s3')
+    bucket = s3_resource.Bucket(bucket_name)
+    bucket.objects.all().delete()
+    bucket.delete()
+
+def call_script(repo, branch_name_key_part, stack_suffix, existing_stacks):
+    stack_name = stack_set_name(repo, branch_name_key_part, stack_suffix)
+    if stack_name in existing_stacks:
+        cloudformation.delete_stack(StackName=stack_name)
+        try:
+            waiter = cloudformation.get_waiter('stack_delete_complete')
+            waiter.wait(StackName=stack_name)
+        except botocore.exceptions.ClientError as ex:
+            print("...waiting too long")
+    return
+
+def delete_stack(repo, branch_name):
+    branch_name_prefix_part, branch_name_key_part = get_branch_name_key_part(branch_name)
+
+    existing_stacks = list(map(lambda stack:stack['StackName'], cloudformation.describe_stacks()['Stacks']))
+
+    t1 = threading.Thread(target=call_script, args=(repo, branch_name_key_part, 'DEV', existing_stacks))
+    t2 = threading.Thread(target=call_script, args=(repo, branch_name_key_part, 'UAT', existing_stacks))
+    t3 = threading.Thread(target=call_script, args=(repo, branch_name_key_part, 'PROD', existing_stacks))
+
+    t1.start()
+    t2.start()
+    t3.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+
+    stack_name = configs[repo]['stack-name-prefix'] + '-' + branch_name_key_part
+    if stack_name in existing_stacks:
+        delete_bucket(get_artifacts_bucket_name(repo, branch_name_key_part))
+        cloudformation.delete_stack(StackName=stack_name)
+
+        s3.download_file(webhhoks_configs_s3_bucket_name, branches_deployment_state_file, '/tmp/' + branches_deployment_state_file)
+        new_branches_deployment_state_file = 'new_' + branches_deployment_state_file
+
+        with open('/tmp/' + branches_deployment_state_file, 'r') as file:
+            branches_deployment_state = json.load(file)
+            branches_deployment_state[repo].remove(branch_name)
+            with open('/tmp/' + new_branches_deployment_state_file, 'w') as file2:
+                json.dump(branches_deployment_state, file2)
+            s3.upload_file('/tmp/' + new_branches_deployment_state_file, webhhoks_configs_s3_bucket_name, branches_deployment_state_file)
+
+        os.remove('/tmp/' + branches_deployment_state_file)
+        os.remove('/tmp/' + new_branches_deployment_state_file)
 
 
 def write_key(filename, contents):
@@ -113,15 +281,13 @@ def zip_repo(repo_path, repo_name):
 def push_s3(filename, repo_name, outputbucket):
     s3key = '%s/%s' % (repo_name, filename.replace('/tmp/', ''))
     logger.info('pushing zip to s3://%s/%s' % (outputbucket, s3key))
+    logger.info('THIS IS A CUSTOM MESSAGE TO TEST LAMBDA UPLOAD TO S3')
     data = open(filename, 'rb')
     s3.put_object(Bucket=outputbucket, Body=data, Key=s3key)
     logger.info('Completed S3 upload...')
 
 
 def lambda_handler(event, context):
-    keybucket = event['context']['key-bucket']
-    outputbucket = event['context']['output-bucket']
-    pubkey = event['context']['public-key']
     # Source IP ranges to allow requests from, if the IP is in one of these the request will not be chacked for an api key
     ipranges = []
     for i in event['context']['allowed-ips'].split(','):
@@ -149,6 +315,7 @@ def lambda_handler(event, context):
             if k1 == k2:
                 secure = True
     # TODO: Add the ability to clone TFS repo using SSH keys
+    logger.info(json.dumps(event))
     try:
         full_name = event['body-json']['repository']['full_name']
     except KeyError:
@@ -173,29 +340,20 @@ def lambda_handler(event, context):
             else:
                 branch_name = 'master'
             repo_name = full_name + '/branch/' + branch_name
-    try:
-        remote_url = event['body-json']['project']['git_ssh_url']
-    except Exception:
-        try:
-            remote_url = 'git@'+event['body-json']['repository']['links']['html']['href'].replace('https://', '').replace('/', ':', 1)+'.git'
-        except:
-            remote_url = event['body-json']['repository']['ssh_url']
     repo_path = '/tmp/%s' % repo_name
-    creds = RemoteCallbacks(credentials=get_keys(keybucket, pubkey), )
-    try:
-        repository_path = discover_repository(repo_path)
-        repo = Repository(repository_path)
-        logger.info('found existing repo, using that...')
-    except Exception:
-        logger.info('creating new repo for %s in %s' % (remote_url, repo_path))
-        repo = create_repo(repo_path, remote_url, creds)
-    pull_repo(repo, branch_name, remote_url, creds)
-    zipfile = zip_repo(repo_path, repo_name)
-    push_s3(zipfile, repo_name, outputbucket)
+
+    logger.info('INFOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO')
+    logger.info(repo_name)
+    logger.info(branch_name)
+
+    if event['params']['header']['X-GitHub-Event'] == 'delete':
+        delete_stack(os.path.basename(full_name), branch_name)
+    else:
+        create_stack(os.path.basename(full_name), branch_name)
+
     if cleanup:
         logger.info('Cleanup Lambda container...')
         shutil.rmtree(repo_path)
-        os.remove(zipfile)
         os.remove('/tmp/id_rsa')
         os.remove('/tmp/id_rsa.pub')
     return 'Successfully updated %s' % repo_name
